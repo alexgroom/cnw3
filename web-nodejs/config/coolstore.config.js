90bde3f454acfa839be4dcab1d56a0e0f1341800
Alex Groom <agroom@redhat.com>
2020-05-17 14:28:11 +0100

    Remove broker config

--- web-nodejs/config/coolstore.config.js
+++ web-nodejs/config/coolstore.config.js
@@ -1,8 +1,7 @@
 var config = {
     API_ENDPOINT: 'gateway-coolstore.' + process.env.OPENSHIFT_BUILD_NAMESPACE,
     SECURE_API_ENDPOINT: 'secure-gateway-coolstore.' + process.env.SECURE_COOLSTORE_GW_SERVICE,
-    SSO_ENABLED: process.env.SSO_URL ? true : false,
-    BROKER_ENDPOINT: {}
+    SSO_ENABLED: process.env.SSO_URL ? true : false
 };
 
 if (process.env.COOLSTORE_GW_ENDPOINT != null) {
@@ -16,13 +15,6 @@ if (process.env.SECURE_COOLSTORE_GW_ENDPOINT != null) {
     config.SECURE_API_ENDPOINT = process.env.SECURE_COOLSTORE_GW_ENDPOINT;
 } else if (process.env.SECURE_COOLSTORE_GW_SERVICE != null) {
     config.SECURE_API_ENDPOINT = process.env.SECURE_COOLSTORE_GW_SERVICE + '.' + process.env.OPENSHIFT_BUILD_NAMESPACE;
-}
-
-if (process.env.COOLSTORE_BROKER_ENDPOINT != null) {
-    config.BROKER_ENDPOINT = process.env.COOLSTORE_BROKER_ENDPOINT;
-} else 
-{
-    config.BROKER_ENDPOINT = null;
 }
 
 module.exports = config;
