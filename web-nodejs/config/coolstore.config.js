628841583fe3efe6e456afd586e7bfafebf88149
Alex Groom <agroom@redhat.com>
2020-05-17 13:46:19 +0100

    declare endpoint

--- web-nodejs/config/coolstore.config.js
+++ web-nodejs/config/coolstore.config.js
@@ -1,7 +1,8 @@
 var config = {
     API_ENDPOINT: 'gateway-coolstore.' + process.env.OPENSHIFT_BUILD_NAMESPACE,
     SECURE_API_ENDPOINT: 'secure-gateway-coolstore.' + process.env.SECURE_COOLSTORE_GW_SERVICE,
-    SSO_ENABLED: process.env.SSO_URL ? true : false
+    SSO_ENABLED: process.env.SSO_URL ? true : false,
+    BROKER_ENDPOINT: {}
 };
 
 if (process.env.COOLSTORE_GW_ENDPOINT != null) {
