c74fcab9d49fbeda264464d979273ef5688e7ac2
Alex Groom <agroom@redhat.com>
2020-12-07 18:05:48 +0000

    Change graphics for products

--- catalog-go/handlers.go
+++ catalog-go/handlers.go
@@ -30,14 +30,15 @@ func GetProducts(w http.ResponseWriter, r *http.Request){
 		w.Write([]byte("Misbehavior of the Catalog GoLang Service\n"))
 	} else {
 		products := Products{
-			Product{ ItemId: "329299", Name: "Red Fedora", Description: "OFFICIAL RED HAT FEDORA", Price: 34.99},
-			Product{ ItemId: "329199", Name: "Forge Laptop Sticker", Description: "JBOSS COMMUNITY FORGE PROJECT STICKER", Price: 8.50},
-			Product{ ItemId: "165613", Name: "Solid Performance Polo", Description: "MOISTURE-WICKING, ANTIMICROBIAL 100% POLYESTER DESIGN WICKS FOR LIFE OF GARMENT. NO-CURL, RIB-KNIT COLLAR...", Price: 17.80},
-			Product{ ItemId: "165614", Name: "Ogio Caliber Polo", Description: "MOISTURE-WICKING 100% POLYESTER. RIB-KNIT COLLAR AND CUFFS; OGIO JACQUARD TAPE INSITEM_IDE NECK; BAR-TACKED THREE-BUTTON PLACKET WITH...", Price: 28.75},
-			Product{ ItemId: "165954", Name: "16 oz. Vortex Tumbler", Description: "DOUBLE-WALL INSULATED, BPA-FREE, ACRYLIC CUP. PUSH-ON LITEM_ID WITH THUMB-SLITEM_IDE CLOSURE; FOR HOT AND COLD BEVERAGES. HOLDS 16 OZ. HAND WASH ONLY. IMPRINT. CLEAR.", Price: 6.00},
-			Product{ ItemId: "444434", Name: "Pebble Smart Watch", Description: "SMART GLASSES AND SMART WATCHES ARE PERHAPS TWO OF THE MOST EXCITING DEVELOPMENTS IN RECENT YEARS.", Price: 24.00},
-			Product{ ItemId: "444435", Name: "Oculus Rift", Description: "THE WORLD OF GAMING HAS ALSO UNDERGONE SOME VERY UNIQUE AND COMPELLING TECH ADVANCES IN RECENT YEARS. VIRTUAL REALITY...", Price: 106.00},
-			Product{ ItemId: "444436", Name: "Lytro Camera", Description: "CONSUMERS WHO WANT TO UP THEIR PHOTOGRAPHY GAME ARE LOOKING AT NEWFANGLED CAMERAS LIKE THE LYTRO FIELD CAMERA, DESIGNED TO ...", Price: 44.30},
+			Product{ ItemId: "100000", Name: "Red Fedora", Description: "Official Red Hat Fedora", Price: 34.99},
+			Product{ ItemId: "329299", Name: "Quarkus T-shirt", Description: "This updated unisex essential fits like a well-loved favorite, featuring a crew neck, short sleeves and designed with superior combed and ring- spun cotton.", Price: 8.50},
+			Product{ ItemId: "329199", Name: "Pronounced Kubernetes", Description: "Kubernetes is changing how enterprises work in the cloud. But one of the biggest questions people have is: How do you pronounce it?", Price: 17.80},
+			Product{ ItemId: "165613", Name: "Knit socks", Description: "Your brand will get noticed on these full color knit socks. Imported.", Price: 28.75},
+			Product{ ItemId: "165614", Name: "Quarkus H2Go water bottle", Description: "Sporty 16. 9 oz double wall stainless steel thermal bottle with copper vacuum insulation, and threaded insulated lid. Imprinted. Imported.", Price: 6.00},
+			Product{ ItemId: "165954", Name: "Patagonia Refugio pack 28L", Description: "Made from 630-denier 100% nylon (50% recycled/50% high-tenacity) plain weave; lined with 200-denier 100% recycled polyester. ...", Price: 24.00},
+			Product{ ItemId: "444434", Name: "Red Hat Impact T-shirt", Description: "This 4. 3 ounce, 60% combed ringspun cotton/40% polyester jersey t- shirt features a slightly heathered appearance. The fabric laundered for reduced shrinkage. Next Level brand apparel. Printed.", Price: 106.00},
+			Product{ ItemId: "444435", Name: "Quarkus twill cap", Description: "100% cotton chino twill cap with an unstructured, low-profile, six-panel design. The crown measures 3 1/8 and this features a Permacurv visor and a buckle closure with a grommet.", Price: 44.30},
+			Product{ ItemId: "444437", Name: "Nanobloc Universal Webcam Cover", Description: "NanoBloc Webcam Cover fits phone, laptop, desktop, Pc, MacBook Pro, iMac, ...", Price: 44.30},
 		}
 		
 		// Define Content-Type = application/json
