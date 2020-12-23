757b8f345c99b86ee9edb4e9147e5047143ccbba
Alex Groom <agroom@redhat.com>
2020-12-23 09:10:23 +0000

    Gateway in dotnet

---
+++ gateway-dotnet/gateway/Controllers/ProductsController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace gateway.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static HttpClient catalogHttpClient = new HttpClient();
+        private static HttpClient inventoryHttpClient = new HttpClient();
+
+        private static String catalogApiHost;
+        private static String inventoryApiHost;
+        public static void Config()
+        {
+            // discover the URL of the services we are going to call
+            catalogApiHost = "http://" + 
+                Environment.GetEnvironmentVariable("COMPONENT_CATALOG_HOST") + ":" +
+                Environment.GetEnvironmentVariable("COMPONENT_CATALOG_PORT");
+            
+            inventoryApiHost = "http://" +
+                Environment.GetEnvironmentVariable("COMPONENT_INVENTORY_HOST") + ":" +
+                Environment.GetEnvironmentVariable("COMPONENT_INVENTORY_PORT");
+
+            // set up the Http conection pools
+            inventoryHttpClient.BaseAddress = new Uri(inventoryApiHost);
+            catalogHttpClient.BaseAddress = new Uri(catalogApiHost);
+        }
+
+        
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Products> Get()
+        {            
+            try
+            {
+                // get the product list
+                IEnumerable<Products> productsList = GetCatalog();
+
+                // update each item with their inventory value
+                foreach(Products p in productsList)
+                {
+                    Inventory inv = GetInventory(p.ItemId);
+                    if (inv != null)
+                        p.Availability = new Availability(inv);
+                }    
+
+                return productsList;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Using Catalog service: " + catalogApiHost + " and Inventory service: " + inventoryApiHost);
+                Console.WriteLine("Failure to get data: " + e.Message);
+                // on failures return error
+                return null;
+            }
+        }
+
+        private IEnumerable<Products> GetCatalog()
+        { 
+            var data = catalogHttpClient.GetStringAsync("/api/catalog").Result;
+            return JsonConvert.DeserializeObject<IEnumerable<Products>>(data);
+        }
+        private Inventory GetInventory(string itemId)
+        {
+            var data = inventoryHttpClient.GetStringAsync("/api/inventory/" + itemId).Result;
+            return JsonConvert.DeserializeObject<Inventory>(data);
+        }
+
+    }
+}
