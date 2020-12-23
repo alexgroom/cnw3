a7d14e74b263abd505203a41b29765c00c49353b
Alex Groom <agroom@redhat.com>
2020-12-23 10:43:31 +0000

    CORS enabled

--- gateway-dotnet/gateway/Startup.cs
+++ gateway-dotnet/gateway/Startup.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +24,8 @@ namespace gateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddControllers().AddJsonOptions(options=> 
             {  
                   options.JsonSerializerOptions.IgnoreNullValues = true;
@@ -44,6 +44,11 @@ namespace gateway
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder
+                 .AllowAnyOrigin ()
+                 .AllowAnyHeader ()
+                 .AllowAnyMethod ());
 
             app.UseHealthChecks("/health");
 
