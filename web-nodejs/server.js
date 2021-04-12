edf76edf254da1f28ede9259e1a5f7f4739d62f9
Alex Groom <agroom@redhat.com>
2021-04-12 17:53:20 +0100

    try multiple events

--- web-nodejs/server.js
+++ web-nodejs/server.js
@@ -9,6 +9,43 @@ const cors = require('cors');
 const probe = require('kube-probe');
 const http = require('http')
 
+sendEvent();
+
+// Environment Variables
+const gulp = require('gulp'); // Load gulp
+const gulpfile = require('./gulpfile'); // Loads our config task
+// Kick of gulp 'config' task, which generates angular const configuration
+gulp.series(gulp.task('config'))();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Enable CORS support
+app.use(cors());
+
+// error handling
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something bad happened!');
+});
+
+app.use('/index.html', function(req, res, next)
+{
+  sendEvent();
+  next()
+})
+app.use('/', express.static(path.join(__dirname, 'views')));
+app.use('/app', express.static(path.join(__dirname, 'app')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+// Add a health check
+probe(app);
+
+
+module.exports = app;
+
+function sendEvent()
+{
 // notify the event broker we have started
 console.log("Checking for broker " + process.env.K_SINK);
 if (process.env.K_SINK != null)
@@ -34,33 +71,5 @@ if (process.env.K_SINK != null)
       
       req.write("")
       req.end()
-
+    }
 }
-
-// Environment Variables
-const gulp = require('gulp'); // Load gulp
-const gulpfile = require('./gulpfile'); // Loads our config task
-// Kick of gulp 'config' task, which generates angular const configuration
-gulp.series(gulp.task('config'))();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-// Enable CORS support
-app.use(cors());
-
-// error handling
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something bad happened!');
-});
-
-app.use('/', express.static(path.join(__dirname, 'views')));
-app.use('/app', express.static(path.join(__dirname, 'app')));
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
-
-// Add a health check
-probe(app);
-
-
-module.exports = app;
