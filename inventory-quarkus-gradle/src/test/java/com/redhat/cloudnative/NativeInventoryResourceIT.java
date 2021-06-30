d5e5f3366e58b92010e03edad0af7bac28334e0c
Alex Groom <agroom@redhat.com>
2021-06-30 17:18:32 +0100

    Add gradle variant for inventory

---
+++ inventory-quarkus-gradle/src/test/java/com/redhat/cloudnative/NativeInventoryResourceIT.java
@@ -0,0 +1,8 @@
+package com.redhat.cloudnative;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+public class NativeInventoryResourceIT extends InventoryResourceTest {
+
+    // Execute the same tests but in native mode.
+}
